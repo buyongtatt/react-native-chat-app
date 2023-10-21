3edcad1cc9aead4def0333cff126b62ffaa60cfd
buyongtatt <yongtatt981213@hotmail.com>
2023-10-21 21:08:18 +0800

    commit

---
+++ config/firebase.js
@@ -0,0 +1,17 @@
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
+import Constants from "expo-constants";
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: Constants.expoConfig.extra.apiKey,
+  authDomain: Constants.expoConfig.extra.authDomain,
+  projectId: Constants.expoConfig.extra.projectId,
+  storageBucket: Constants.expoConfig.extra.storageBucket,
+  messagingSenderId: Constants.expoConfig.extra.messagingSenderId,
+  appId: Constants.expoConfig.extra.appId,
+};
+initializeApp(firebaseConfig);
+export const auth = getAuth();
+export const database = getFirestore();
